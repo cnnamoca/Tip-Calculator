9c05c6a9047e6c61023011235672308218f1b615
Carlo <carlo.namoca@gmail.com>
2017-10-13 11:08:04 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Carlo Namoca on 2017-10-13.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
